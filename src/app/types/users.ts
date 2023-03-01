c1380c82bc4f01fa8e1ada084f0901ab4935f6b2
Vlad Prodan <vladprodan6@gmail.com>
2023-03-01 08:55:41 +0100

    types

--- src/app/types/users.ts
+++ src/app/types/users.ts
@@ -3,6 +3,25 @@ export type UsersState = {
 }
 
 export interface User {
+	id: number
 	name: string
-	age: number
+	username: string
+	email: string
+	address: {
+		street: string
+		suite: string
+		city: string
+		zipcode: string
+		geo: {
+			lat: string
+			lng: string
+		}
+	}
+	phone: string
+	website: string
+	company: {
+		name: string
+		catchPhrase: string
+		bs: string
+	}
 }
