f6147045d5f88810e5b1200e673454babcadd18c
Vlad Prodan <vladprodan6@gmail.com>
2023-03-01 10:11:38 +0100

    ++

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import './App.css'
-import { useGetUsersQuery } from './app/service/user'
 
 function App() {
-	const { data, isLoading, error } = useGetUsersQuery('users')
 
 	return (
 		<div className='App'>
-			
 		</div>
 	)
 }
